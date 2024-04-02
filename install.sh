9bc7aa9f0c31dd8a4f2217480ec4a8885fcd9e83
͚̤̖͕̪̝͇̟͈̙͉̒͐̎͋͂͛̄̋́̈̇͐̂̋ͅ-̬̰͇̮̟̖̞̬̙̙̯͎͔̣̎̐͋̈́́̒̇̍̑̚H̰͍̙͍̘̘͙͔̊͐̀̀͗̊̚̚Ɔ͖̞͕̭̖̞̠̘̪̫̲̥̣̬͎͈̀̇͋͛͆̔̇̉͑̇̾͌͛̎̍̎Ś͙̯͈͕̱͉̤̭̏͋̃̉͒̑̃̏̾̿̇̃̒͋͌ͅN͉͚̰̫̞̪͈̭̤̞͔͓͎̣̄̓̅̋̈́̉͆̊-̪͈͈̭͎͍̭̥̭͈̣̫̙͍̉̂́̋͆̉̾̄͋̇͌̈̏́̈̚.͓̗̰͙̖̥̠̭̱̙̯̃̀͗͆̃̈́͂͂͐͌͐ <117274957+Nkipohcs@users.noreply.github.com>
2024-04-03 01:26:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/Nkipohcs/.ft_lock/main/ft_lock -O ~/.ft_lock
+wget https://raw.githubusercontent.com/Nkipohcs/.ft__lock/main/ft__lock -O ~/.ft_lock
 
-chmod +x ~/.ft_lock
+chmod +x ~/.ft__lock
 
 SHELL_RC="$HOME/.bashrc"
 if [ -f "$HOME/.zshrc" ]; then
@@ -10,11 +10,11 @@ if [ -f "$HOME/.zshrc" ]; then
 fi
 
 
-if ! grep -q "alias ft_lock='sh ~/.ft_lock'" "$SHELL_RC"; then
-    echo "alias ft_lock='sh ~/.ft_lock'" >> "$SHELL_RC"
-    echo "Alias ft_lock ajouté à $SHELL_RC"
+if ! grep -q "alias ft__lock='sh ~/.ft_lock'" "$SHELL_RC"; then
+    echo "alias ft__lock='sh ~/.ft_lock'" >> "$SHELL_RC"
+    echo "Alias ft__lock ajouté à $SHELL_RC"
 else
-    echo "L'alias ft_lock est déjà présent dans $SHELL_RC"
+    echo "L'alias ft__lock est déjà présent dans $SHELL_RC"
 fi
 
 rm install.sh
